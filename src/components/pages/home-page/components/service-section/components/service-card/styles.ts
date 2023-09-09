2e5180f68d23a8729ad81288fd1e9630103daadd
Murilo Gruppi <murilo.castro@ccc.ufcg.edu.br>
2023-09-09 18:44:43 -0300

    feat: implements services section

---
+++ src/components/pages/home-page/components/service-section/components/service-card/styles.ts
@@ -0,0 +1,40 @@
+import { styled } from 'styled-components';
+
+import theme from '@/styles/theme';
+
+export const Container = styled.div`
+  height: 100%;
+
+  max-width: 35rem;
+  min-height: 25rem;
+
+  padding: 3.4rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  flex-direction: column;
+
+  gap: 1.6rem;
+
+  color: ${theme.colors.primary.dark};
+
+  border-radius: ${theme.borderRadius};
+  border: 2px solid ${theme.colors.primary.dark};
+
+  @media (max-width: 1280px) {
+    padding: 2.4rem;
+  }
+`;
+
+export const ServiceTitle = styled.h4`
+  font-weight: 500;
+  text-align: center;
+`;
+
+export const ServiceDescription = styled.p`
+  font-size: 1.4rem;
+
+  text-align: justify;
+`;
---
+++ src/components/pages/home-page/components/service-section/service-section.tsx
@@ -0,0 +1,56 @@
+import { FC } from 'react';
+
+import { Button } from '@/components/common/button/button';
+import { DesktopIcon } from '@/components/icon/desktop-icon';
+import { MaintanceIcon } from '@/components/icon/maintance-icon';
+import { MobileIcon } from '@/components/icon/mobile-icon';
+import { SEOIcon } from '@/components/icon/seo-icon';
+
+import { ServiceCard } from './components/service-card/service-card';
+import { FIRST_DESCRIPTION, SECOND_DESCRIPTION, SERVICES } from './constants';
+import {
+  ButtonWrapper,
+  Container,
+  Content,
+  Description,
+  ServiceCardsWrapper,
+  ServicesContainer,
+  Title,
+} from './styles';
+
+export const ServiceSection: FC = () => (
+  <Container>
+    <Content>
+      <Title>Serviços</Title>
+      <ServicesContainer>
+        <Description>{FIRST_DESCRIPTION}</Description>
+        <ServiceCardsWrapper>
+          <ServiceCard
+            title={SERVICES.web.title}
+            description={SERVICES.web.description}
+            renderIcon={() => <DesktopIcon />}
+          />
+          <ServiceCard
+            title={SERVICES.mobile.title}
+            description={SERVICES.mobile.description}
+            renderIcon={() => <MobileIcon />}
+          />
+          <ServiceCard
+            title={SERVICES.seo.title}
+            description={SERVICES.seo.description}
+            renderIcon={() => <SEOIcon />}
+          />
+          <ServiceCard
+            title={SERVICES.maintenance.title}
+            description={SERVICES.maintenance.description}
+            renderIcon={() => <MaintanceIcon />}
+          />
+        </ServiceCardsWrapper>
+        <Description>{SECOND_DESCRIPTION}</Description>
+        <ButtonWrapper>
+          <Button title="Quero saber mais" />
+        </ButtonWrapper>
+      </ServicesContainer>
+    </Content>
+  </Container>
+);
