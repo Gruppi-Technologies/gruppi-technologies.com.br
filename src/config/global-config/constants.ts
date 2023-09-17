a9de8969e787a468788e1f724713c8f79ac4cc97
Murilo Gruppi <murilo.castro@ccc.ufcg.edu.br>
2023-09-17 00:04:17 -0300

    feat: add business phone number

--- src/config/global-config/constants.ts
+++ src/config/global-config/constants.ts
@@ -2,4 +2,4 @@ export const PROJECT_TITLE = 'Gruppi Technologies';
 export const PROJECT_DESCRIPTION = 'Opções para além da imaginação: Transformando ideias em realidade';
 export const DEFAULT_WHATSAPP_MESSAGE =
   'Olá! Estou buscando mais informações sobre os serviços oferecidos pela Gruppi Technologies. Poderia me ajudar com algumas dúvidas?';
-export const WHATSAPP_LINK = `https://wa.me/+5511941341979?text=${DEFAULT_WHATSAPP_MESSAGE}`;
+export const WHATSAPP_LINK = `https://wa.me/+5511915801617?text=${DEFAULT_WHATSAPP_MESSAGE}`;
