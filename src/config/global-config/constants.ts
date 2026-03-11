a63c6faf6ff1d1a9ba1f512189ba071770327317
Murilo Gruppi <murilo.gruppidecastro@gmail.com>
2026-03-11 14:00:48 -0300

    feat: implements rate limit for send email

--- src/config/global-config/constants.ts
+++ src/config/global-config/constants.ts
@@ -2,4 +2,4 @@ export const PROJECT_TITLE = 'Gruppi Technologies';
 export const PROJECT_DESCRIPTION = 'Opções para além da imaginação: Transformando ideias em realidade';
 export const WHATSAPP_NUMBER = '(11) 91951-6221';
 export const MAIL_ADDRESS = 'gruppitechnologies.contato@gmail.com';
-export const WHATSAPP_LINK = `https://wa.me/message/HMYZSV7WS3AFA1`;
+export const WHATSAPP_LINK = `https://wa.me/5511919516221?text=Olá, gostaria de saber mais sobre os seus serviços.`;
